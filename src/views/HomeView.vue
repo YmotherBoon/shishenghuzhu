3be178e0ec614ff37ecad0566b052429d3454ec7
李志伟 <2033258094@qq.com>
2023-04-09 17:35:13 +0800

    add files

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <div>
+      毕业设计
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  components: {
+
+  }
+}
+</script>
+<style scoped>
+.home{
+  height: 100%;
+  background: aqua;
+}
+</style>
